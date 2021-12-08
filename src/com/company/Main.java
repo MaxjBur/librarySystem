c05aa1b03e7c515e5e63d56c648a742ccd6bd4ff
Max Burton-Phillips <MB211187@reigate.ac.uk>
2021-12-08 09:38:23 +0000

    Added Login Feature and Fixed File Writing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,95 @@
 package com.company;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.File;
+
 // good variable names, nice ☺
 public class Main {
     private static ArrayList<String> books = new ArrayList<>();
-//I need to find the text file and make sure it works
+
+    //I need to find the text file and make sure it works
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many books would you like to enter?");
-        int numBooks=scanner.nextInt();
-        //createFile();
-        for (int i = 0; i <numBooks ; i++) {
+        login();
+        int numOfBooks = numOfBooks();
+
+
+        for (int i = 0; i < numOfBooks; i++) {
             books.add(getBookDetails());
+            System.out.println(books);
+
+        }
+        int menuOption;
+        while (true) {
+            menuOption = menu();
+            if (menuOption == 1) {
+                createFile();
+            } else if (menuOption == 2) {
+                writeToFile(books);
+            } else if (menuOption == 3) {
+                break;
+            }
+
         }
 
-        writeToFile(books);
+    }
 
+    public static void login() {
+        File file = new File("users.txt");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            Scanner scanner1 = new Scanner(file);
+            String userName;
+            String line;
+            String password;
+            boolean x = true;
+            while (x) {
+                System.out.println("What is your username?");
+                userName = scanner.nextLine();
+                int lineNum = 0;
+                while (scanner1.hasNextLine()) {
+                     line = scanner1.nextLine();
+                    lineNum++;
+                    if (line.contains(userName)) {
+                        System.out.println("Username is correct");
+                        System.out.println("What is your password?");
+                         password = scanner.next();
+                        if (line.contains(password)) {
+                            System.out.println("password is correct");
+                            x = false;
+                        } else {
+                            System.out.println("Incorrect password");
+                        }
+                    } else {
+                        System.out.println("Incorrect username");
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static int menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select an Option 1-3:");
+        System.out.println("1) Create File");
+        System.out.println("2) Write Book Details to File");
+        System.out.println("3) Quit Program");
+        int menuOption = scanner.nextInt();
+        return menuOption;
+    }
+
+    public static int numOfBooks() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many books would you like to enter?");
+        int numBooks = scanner.nextInt();
+        return (numBooks);
     }
 
     public static String getBookDetails() {
@@ -44,6 +114,14 @@ public class Main {
                 System.out.println("File created: " + fileObj.getName());
             } else {
                 System.out.println("File already exists.");
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Would you like to delete the file 1=Y, 2=N?");
+                int deleteFile = scanner.nextInt();
+                if (deleteFile == 1) {
+                    fileObj.delete();
+                    System.out.println("File has been deleted");
+                }
+
             }
         } catch (IOException e) {
             System.out.println("An error occurred."); // show which error
@@ -52,11 +130,14 @@ public class Main {
 
         }
     }
-    public static void writeToFile(ArrayList<String> bookList ){
+
+    public static void writeToFile(ArrayList<String> bookList) {
         try {
             FileWriter myWriter = new FileWriter("bookfile.txt");
-            myWriter.write(String.valueOf(bookList));
-        }catch (IOException e){
+            //myWriter.write(String.valueOf(bookList));
+            myWriter.write(String.valueOf(books));
+            myWriter.close();
+        } catch (IOException e) {
             System.out.println("An error has occurred.");
             e.printStackTrace();
         }
